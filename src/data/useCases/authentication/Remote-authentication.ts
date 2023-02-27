a5593edf340854c0fe0f1927b622723fd6c220fe
André Abreu de Sá <andre.abreu.ufmg@gmail.com>
2023-02-27 23:00:44 +0000

    feat: ensure remoteAuthentication return Account on success

--- src/data/useCases/authentication/Remote-authentication.ts
+++ src/data/useCases/authentication/Remote-authentication.ts
@@ -2,22 +2,22 @@ import { HttpStastusCode } from "@/data/protocols/http/Http-response";
 import { InvalidCredentialError } from "@/domains/errors/Invalid-credentiral-error";
 import { UnespectedError } from "@/domains/errors/Unexpected-error";
 import { AccountModel } from "@/domains/models/Acount-models";
-import { AuthenticationParams } from "@/domains/useCases/Authentication";
+import { Authentication, AuthenticationParams } from "@/domains/useCases/Authentication";
 import { HttPostClient } from "../../protocols/http/Http-post-client";
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
     constructor(
         private readonly url: string,
         private readonly httpPostClient: HttPostClient<AuthenticationParams, AccountModel>
     ) {}
 
-    async auth (params: AuthenticationParams): Promise<void> {
+    async auth (params: AuthenticationParams): Promise<AccountModel> {
         const httpResponse = await this.httpPostClient.post({
             url : this.url,
             body : params
         })
         switch(httpResponse.statusCode) {
-            case HttpStastusCode.ok: break
+            case HttpStastusCode.ok: return httpResponse.body
             case HttpStastusCode.unathorized: throw new InvalidCredentialError()
              default: throw new UnespectedError()
         }
--- src/domains/test/Mock-authentication.ts
+++
@@ -1,7 +0,0 @@
-import { AuthenticationParams } from "../useCases/Authentication";
-import faker from "faker"
-
-export const MockAuthentication = (): AuthenticationParams => ({
-    email: faker.internet.email(), 
-    passaword: faker.internet.password() 
-});
